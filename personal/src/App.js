bf7a18fc537323411e4633373cf4aa3ceba61347
Jose Varela <joselcvarela@gmail.com>
2018-03-24 08:25:21 +0000

    Setup new personal webpage

---
+++ personal/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="triangle-left"></div>
+        <div className="triangle-right"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
